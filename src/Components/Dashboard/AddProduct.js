7dd05c8aa6ef0ac5181d98c176bcf4db8b21e01a
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-05-25 04:04:09 +0600

    added product

--- src/Components/Dashboard/AddProduct.js
+++ src/Components/Dashboard/AddProduct.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        const url = `http://localhost:5000/review`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                window.alert('successfully Review add');
+                window.location.reload();
+
+                // e.target.reset();
+                console.log(data);
+
+            })
+    };
     return (
         <div>
             <h1 className='text-2xl font-bold'>Add Product</h1>
+
+            <div>
+            
+
+
+                <div className='border lg:m-20 p-5'>
+                    <form className='m-5 d-flex mx-auto flex-column w-50' onSubmit={handleSubmit(onSubmit)}>
+                        <input placeholder='Enter Your Name' className='mt-3 w-full border p-3' {...register("Name",)} />
+                        <input placeholder='Enter Your Email' className='mt-3 w-full border p-3' {...register("Email",)} />
+
+                        <textarea placeholder='Writen your text-' className='mt-3 w-full border p-3' {...register("dec")} />
+                        <input placeholder='Img URL' className='mt-3 w-full border p-3 mb-3' type="text" {...register("img")} />
+                        <input type="submit" className='btn' />
+                    </form>
+                </div>
+
+            </div>
         </div>
     );
 };
