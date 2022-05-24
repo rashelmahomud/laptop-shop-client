fe308b792fdca947c7259f800d4727a4a2ffbe08
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-05-25 03:13:29 +0600

    added dainamic reviews

--- src/Components/Dashboard/AddReview.js
+++ src/Components/Dashboard/AddReview.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddReview = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        const url = `http://localhost:5000/review`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                window.alert('quantity added');
+                window.location.reload();
+
+                // e.target.reset();
+                console.log(data);
+
+            })
+    };
     return (
         <div>
             <h1 className='font-bold text-2xl'>Add Your Review..</h1>
+
+
+            <div className='border lg:m-20 p-5'>
+                <form className='m-5 d-flex mx-auto flex-column w-50' onSubmit={handleSubmit(onSubmit)}>
+                    <input placeholder='Enter Your Name' className='mt-3 w-full border p-3' {...register("Name",)} />
+                    <input placeholder='Enter Your Email' className='mt-3 w-full border p-3' {...register("Email",)} />
+
+                    <textarea placeholder='Writen your text-' className='mt-3 w-full border p-3' {...register("dec")} />
+                    <input placeholder='Img URL' className='mt-3 w-full border p-3 mb-3' type="text" {...register("img")} />
+                    <input type="submit" className='btn' />
+                </form>
+            </div>
+
         </div>
     );
 };
--- src/Components/Page/CustomerReviews/CustomerReviews.js
+++ src/Components/Page/CustomerReviews/CustomerReviews.js
@@ -4,7 +4,7 @@ import CustomerReview from './CustomerReview';
 const CustomerReviews = () => {
     const [reviews, setReviews] = useState([]);
     useEffect( () => {
-        fetch('reviews.json')
+        fetch('http://localhost:5000/review')
         .then(res =>res.json())
         .then(data => setReviews(data))
     },[])
