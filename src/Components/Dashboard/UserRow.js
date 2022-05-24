ec31ec4a27e02374a344c4c5a400a84c7bfb9588
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-05-24 13:04:24 +0600

    added a admin

--- src/Components/Dashboard/UserRow.js
+++ src/Components/Dashboard/UserRow.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-const UserRow = ({user,index}) => {
-    const {email} =user;
+const UserRow = ({ user, index }) => {
+    const { email } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('amake admin', data);
+            })
+    }
 
     return (
-      
-            <tr>
-                <th>{index +1}</th>
-                <td>{email}</td>
-                <td><button className='btn btn-primary text-white'>Make Admin</button></td>
-                <td><button className='btn bg-red-600'>Delete</button></td>
-            </tr>
-        
+
+        <tr>
+            <th>{index + 1}</th>
+            <td>{email}</td>
+            <td><button onClick={makeAdmin} className='btn btn-primary text-white'>Make Admin</button></td>
+            <td><button className='btn bg-red-600'>Delete</button></td>
+        </tr>
+
     );
 };
 
