acb3963329021875c2fdcf008b1159a23f08b7ac
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-09-21 14:23:58 +0600

    added some work

--- src/Components/Page/Banner.js
+++ src/Components/Page/Banner.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import img from '../../assects/image/fsd.png';
-import imgs from '../../assects/image/banner.png';
 import './Banner.css';
 
 const Banner = () => {
     return (
 
-        // <div>
-        //     <div className="banner-container">
-        //         <div className="banner-image">
-        //             <div className="title-container text-center">
-        //                 <div className="title">
-
-        //                     <div data-aos="fade-up"
-        //                         data-aos-duration="1000">
-        //                         <h1 className="text-white font-bold">Welcome to Our Shops</h1>
-        //                         <h1 className="text-white">Hello Dear Customers!</h1>
-        //                     </div>
-        //                     <h6 data-aos="flip-left"
-        //                         data-aos-duration="1000" className="text-white my-5 text-2xl font-bold text-primary">Laptoop parts is a simply dummy text of the printing and typesetting industry. <br /> Our Shops is a best and Unice all Products..</h6>
-        //                     <button className="btn btn-primary text-white">Explore</button>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
         <div className="banner-image hero min-h-screen hellotop mx-auto">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img data-aos="flip-right"
--- src/Components/Shared/ContactUs/ContactUs.js
+++ src/Components/Shared/ContactUs/ContactUs.js
@@ -1,26 +1,32 @@
 import React from 'react';
-// import contactBg from '../../../assets/images/appointment.png';
-
+import contact from '../../../assects/contact.png';
 const ContactUs = () => {
     return (
-        <section className='mt-20 bg-base-200' 
-            // style={{
-            //     background: `url(${contactBg})`
-            // }}
-        >
-            <div className='p-10'>
-                <div className='grid justify-items-center mb-5'>
-                    <h1 className='text-primary text-2xl font-bold'>contact Us</h1>
-                    <h4 className=' text-5xl font-bold'>Say Conected with us</h4>
-                </div>
-                <div className='grid justify-items-center'>
-                    <input className='w-80 rounded-lg p-3' type="text" placeholder='Email Address' /> <br />
-                    <input className='w-80 rounded-lg p-3' type="text" placeholder='subject' /> <br />
-                    <textarea className='w-80 rounded-lg p-3' name="" id="" cols="40" rows="8" placeholder='your massage'></textarea>
-                    <input className='btn btn-primary mt-5 text-white' type="submit" value="submite" />
+        <div>
+            <div className='grid justify-items-center mb-5'>
+                <h1 className='text-primary text-2xl font-bold'>contact Us</h1>
+                <h4 className=' text-4xl font-bold'>Say Conected with us</h4>
+            </div>
+
+            <div className="hero min-h-screen hellotop mx-auto ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <div className='lg:p-10 p-3'>
+                        <div className='grid justify-items-center'>
+                            <input className='lg:w-80 rounded-lg p-3' type="text" placeholder='Email Address' /> <br />
+                            <input className='lg:w-80 rounded-lg p-3' type="text" placeholder='subject' /> <br />
+                            <textarea className='lg:w-80 rounded-lg p-3' name="" id="" cols="40" rows="8" placeholder='your massage'></textarea>
+                            <input className='btn btn-primary mt-5 text-white' type="submit" value="submite" />
+                        </div>
+                    </div>
+
+                    <img className='w-100 lg:mr-20' data-aos="flip-right"
+                        data-aos-duration="2000" src={contact} />
                 </div>
             </div>
-        </section>
+
+        </div>
+
     );
 };
 
