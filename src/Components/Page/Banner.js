5838975227f11878e51caafad200bbd7f44f7ea1
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-09-23 04:26:50 +0600

    adde

--- src/Components/Page/Banner.js
+++ src/Components/Page/Banner.js
@@ -10,10 +10,10 @@ const Banner = () => {
                 <img data-aos="flip-right"
                 data-aos-duration="2000" src={img} />
                 <div className='mr-20 mt-5'>
-                    <h1 className="text-5xl font-bold">Welcome to Our Shops</h1>
-                    <h1 className="text-5xl font-bold">Hello Dear Customers!</h1>
-                    <p className="py-6">Laptoop parts is a simply dummy text of the printing and typesetting industry. <br /> Our Shops is a best and Unice all Products..</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="text-5xl font-bold font-serif">Welcome to Our Shops</h1>
+                    <h1 className="text-5xl font-bold font-sans">Hello Dear Customers!</h1>
+                    <p className="py-6 font-serif">Laptoop parts is a simply dummy text of the printing and typesetting industry. <br /> Our Shops is a best and Unice all Products..</p>
+                    <button className="btn btn-primary font-serif">Get Started</button>
                 </div>
             </div>
         </div>
