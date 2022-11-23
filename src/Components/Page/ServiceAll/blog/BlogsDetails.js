9287ae934619dff54a31d8c09904e5ae8f42f300
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-24 00:34:27 +0600

    added some

--- src/Components/Page/ServiceAll/blog/BlogsDetails.js
+++ src/Components/Page/ServiceAll/blog/BlogsDetails.js
@@ -16,9 +16,18 @@ const BlogsDetails = () => {
 
 
     return (
-        <div className='text-center'>
-            <p>{newblog[0]?.name}</p>
-            <img src={newblog[0]?.img} alt='' />
+        <div className='text-center lg:mx-20'>
+            <img className='mx-auto h-96 lg:w-4/5 p-5' src={newblog[0]?.img} alt='' />
+
+            <div className='text-left lg:mx-32 p-5'>
+                <p className='font-bold text-3xl'>{newblog[0]?.title}</p>
+                <p className='font-bold my-3'>{newblog[0]?.description}</p>
+                <span className='text-3xl font-bold'>Description</span>
+                <p className='font-bold my-3'>{newblog[0]?.dec}</p>
+                <span className='text-3xl font-bold'>Largest Chain of Computer Store in Bangladesh</span>
+                <p className='font-bold my-3'>{newblog[0]?.dec1}</p>
+
+            </div>
 
         </div>
     );
