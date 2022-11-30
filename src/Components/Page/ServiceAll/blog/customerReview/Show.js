82f2e956258fc889ad0c046cd2fb7df398a30736
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-30 14:28:50 +0600

    adde reivw

--- src/Components/Page/ServiceAll/blog/customerReview/Show.js
+++ src/Components/Page/ServiceAll/blog/customerReview/Show.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../../../firebase.init';
-
+import girl from '../../../../../assects/girl.png'
 const Show = ({ d }) => {
     const [user] = useAuthState(auth);
     return (
         <div className='my-10'>
             <div className='flex gap-5'>
-                <img className='rounded-full w-8' src={user.photoURL} alt="" />
+                {
+                    user ? <img className='rounded-full w-8' src={user?.photoURL} alt="" /> : <img className='rounded-full w-12' src={girl} alt="" />
+                }
+
                 <p className='font-bold'>{d.name}</p>
             </div>
             <p>{d.dec}</p>
