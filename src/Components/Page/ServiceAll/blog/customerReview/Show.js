e884db01a1abdb0743d0c045f3118e6fd59c0865
MCTC <rashelmahmudraj1998@gmail.com>
2022-12-25 03:12:57 -0800

    added some

--- src/Components/Page/ServiceAll/blog/customerReview/Show.js
+++ src/Components/Page/ServiceAll/blog/customerReview/Show.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../../../firebase.init';
-import girl from '../../../../../assects/girl.png'
 const Show = ({ d }) => {
     const [user] = useAuthState(auth);
     return (
         <div className='my-10'>
             <div className='flex gap-5'>
-                {
-                    user ? <img className='rounded-full w-8' src={user?.photoURL} alt="" /> : <img className='rounded-full w-12' src={girl} alt="" />
-                }
+                <p className="flex h-screen justify-center items-center w-8 h-8 bg-gray-100 rounded-full">
+                    {
+                        user ? <i class="fa-solid fa-user"></i> : <i class="fa-solid fa-user"></i>
+                    }
+                </p>
 
                 <p className='font-bold'>{d.name}</p>
             </div>
